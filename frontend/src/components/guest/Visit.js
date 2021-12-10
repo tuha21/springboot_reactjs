c7d68b023c8a3ceb12503672b03ba41e2f4d8fa6
TuanNguyen21 <nguyentuan21012001@gmail.com>
2021-12-10 15:08:26 +0700

    update Profile, Visit, About

--- frontend/src/components/guest/Visit.js
+++ frontend/src/components/guest/Visit.js
@@ -14,7 +14,7 @@ const Visit = () => {
             <div class="row pt-5">
                 <div className="col-8">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10232.85400687463!2d105.74049825311074!3d21.037816830332034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b991d80fd5%3A0x53cefc99d6b0bf6f!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1638698324774!5m2!1svi!2s"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.301725264612!2d106.6602916153254!3d10.788187061925264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ecda427034d%3A0xfc0364341b2923b!2zNzU2IEPDoWNoIE3huqFuZyBUaMOhbmcgVMOhbSwgUGjGsOG7nW5nIDYsIFTDom4gQsOsbmgsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCA3MDAwMDAsIFZp4buHdCBOYW0!5e0!3m2!1svi!2shk!4v1639122667230!5m2!1svi!2shk"
                         width="100%"
                         height="600"
                         allowfullscreen=""
@@ -28,34 +28,12 @@ const Visit = () => {
                                 <FaMapMarkerAlt />
                             </div>
                             <div className="col d-grid" style={{ fontFamily: "sans-serif" }}>
-                                <b>SGES Trịnh Văn Bô</b>
-                                <span>FĐịa chỉ: PT Polytechnic Trịnh Văn Bô</span>
-                                <span>SĐT: 0966112738</span>
-                            </div>
-                        </Link>
-                        <Link path="#" className="row p-2 mb-4">
-                            <div className="col-1">
-                                <FaMapMarkerAlt />
-                            </div>
-                            <div className="col d-grid" style={{ fontFamily: "sans-serif" }}>
-                                <b>SGES Trần Thái Tông</b>
+                                <b>SGES Tân Bình</b>
                                 <span>
-                                    Địa chỉ: 24 Ngách 44/64 Trần Thái Tông, Dịch Vọng Hậu, Cầu Giấy,
-                                    Hà Nội
+                                    FĐịa chỉ: 756 Cách mạng tháng 8, Phường 5, Quận Tân Bình, Thành
+                                    phố Hồ Chí Minh
                                 </span>
-                                <span>SĐT: 0966112738</span>
-                            </div>
-                        </Link>
-                        <Link path="#" className="row p-2 mb-4">
-                            <div className="col-1">
-                                <FaMapMarkerAlt />
-                            </div>
-                            <div className="col d-grid" style={{ fontFamily: "sans-serif" }}>
-                                <b>SGES Hoàng Quốc Việt</b>
-                                <span>
-                                    Địa chỉ: 24 Phố Nghĩa Đô, Hoàng Quốc Việt, Cầu Giấy, Hà Nội
-                                </span>
-                                <span>SĐT: 0966112738</span>
+                                <span>SĐT: 0707191381</span>
                             </div>
                         </Link>
                     </div>
