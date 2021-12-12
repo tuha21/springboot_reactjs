93051a311e9ee849b400ec0bb0d6d3aa5518ae06
TuanNguyen21 <nguyentuan21012001@gmail.com>
2021-12-12 08:25:59 +0700

    tuannn - update profile

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
-import prods from "./guestreducer/prods"
-import prodDetail from "./guestreducer/prodDetail"
-import cart from "./guestreducer/cart"
-import products from "./staffreducer/products"
-import productDetail from "./staffreducer/productDetail"
-import myOrders from "./guestreducer/myOrder"
-import auth from "./guestreducer/auth"
+import prods from "./guestreducer/prods";
+import prodDetail from "./guestreducer/prodDetail";
+import cart from "./guestreducer/cart";
+import products from "./staffreducer/products";
+import productDetail from "./staffreducer/productDetail";
+import myOrders from "./guestreducer/myOrder";
+import auth from "./guestreducer/auth";
+import profile from "./profile";
 const myReducer = combineReducers({
     prods,
     prodDetail,
@@ -14,7 +15,8 @@ const myReducer = combineReducers({
     auth,
     //staff
     products,
-    productDetail
+    productDetail,
+    profile,
 });
 
 export default myReducer;
---
+++ frontend/src/reducers/profile.js
@@ -0,0 +1,23 @@
+import { UPDATE_PROFILE } from "./../constants/constants";
+
+const initProfile = {
+    username: "",
+    fullname: "",
+    email: "",
+    photo: "",
+    status: "",
+};
+
+const myReducer = (state = initProfile, action) => {
+    switch (action.type) {
+        case UPDATE_PROFILE:
+            return {
+                ...state,
+                action,
+            };
+        default:
+            return state;
+    }
+};
+
+export default myReducer;
